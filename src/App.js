780e4f351d514f900ee2dc2967ffdecdf627ece5
FernPK <pornnapat5563@gmail.com>
2022-06-03 23:53:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import buttonInfo from './data';
+import $ from 'jquery';
+
+class Power extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    if (this.props.status){
+      $('#power').css('float', 'right');
+    }
+    else {
+      $('#power').css('float', 'left');
+    }
+    return (
+      <div className='switch-wrapper'>
+        <label for='power'>Power</label>
+        <div className='outer-switch' onClick={this.props.toggle}>
+          <div className='switch' id='power'></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Bank extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    if (this.props.status){
+      $('#bank').css('float', 'right');
+    }
+    else {
+      $('#bank').css('float', 'left');
+    }
+    return (
+      <div className='switch-wrapper'>
+        <label for='bank'>Bank</label>
+        <div className='outer-switch' onClick={this.props.power ? this.props.switchBank : ()=>null}>
+          <div className='switch' id='bank'></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Drum extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      'power': true,
+      'bank': 0
+    }
+    this.play = this.play.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.toggleChange = this.toggleChange.bind(this);
+    this.switchBank = this.switchBank.bind(this);
+  }
+  componentDidMount() {
+    //document.addEventListener('keydown', this.handleKeyPress);
+    //$(body).on('keydown', this.handleKeyPress);
+    $('body').keydown(this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    //document.removeEventListener('keydown', this.handleKeyPress);
+    $('body').off('keydown', this.handleKeyPress)
+  }
+  play(item) {
+    console.log(item.id[this.state.bank]);
+    const audio = $('#'+item.key)[0];
+    console.log(item.key);
+    audio.currentTime = 0;
+    audio.play();
+    $('#display').text(item.id[this.state.bank]);
+  }
+  handleKeyPress(event) {
+    for(let i=0; i<buttonInfo.length; i++){
+      if (event.key.toUpperCase() === buttonInfo[i].key){
+        //document.getElementById(buttonInfo[i].id).click();
+        $('#'+buttonInfo[i].id[this.state.bank]).click();
+      }
+    }
+  }
+  toggleChange(){
+    this.setState({
+      'power': !this.state.power
+    });
+    if(this.state.power===true){
+      $('#display').text('');
+    }
+  }
+  switchBank(){
+    if (this.state.bank===0){
+      this.setState({
+        bank: 1
+      });
+    }
+    else {
+      this.setState({
+        bank: 0
+      });
+    }
+  }
+  render() {
+    const buttonMap = buttonInfo.map((item) => {
+      return (
+        <button className='drum-pad' id={item.id[this.state.bank]} key={item.id[this.state.bank]} onClick={this.state.power ? (() => this.play(item)) : ()=>null}>
+          <audio src={item.audio[this.state.bank]} id={item.key} className='clip'></audio>
+          {item.key}
+        </button>
+      );
+    })
+    return(
+      <div id='drum-machine'>
+        <div id='pad'>{buttonMap}</div>
+        <div id='setting'>
+          <Power toggle={this.toggleChange} status={this.state.power}/>
+          <div id='display'></div>
+          {/*<div id='sound'></div>*/}
+          <Bank switchBank={this.switchBank} status={this.state.bank} power={this.state.power}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drum />
+      <p className='credit' id='credit1'>Photo by <a href='https://unsplash.com/@clemono?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Clem Onojeghuo</a> on <a href='https://unsplash.com/s/photos/night-cafe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Unsplash</a></p>
+      <p className='credit' id='credit2'><a href='https://www.freepik.com/photos/black-leather'>Black leather photo created by denamorado - www.freepik.com</a></p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,49 @@
+const buttonInfo = [
+  {
+    id: ['Heater-1', 'Chord-1'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'],
+    key: 'Q'
+  },
+  {
+    id: ['Heater-2', 'Chord-2'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'],
+    key: 'W'
+  },
+  {
+    id: ['Heater-3', 'Chord-3'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'],
+    key: 'E'
+  },
+  {
+    id: ['Heater-4', 'Shaker'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'],
+    key: 'A'
+  },
+  {
+    id: ['Clap', 'Open-HH'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'],
+    key: 'S'
+  },
+  {
+    id: ['Open-HH', 'Closed-HH'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'],
+    key: 'D'
+  },
+  {
+    id: ['Kick-n-Hat', 'Punchy-Kick'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'],
+    key: 'Z'
+  },
+  {
+    id: ['Kick', 'Side-Stick'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'],
+    key: 'X'
+  },
+  {
+    id: ['Closed-HH', 'Snare'],
+    audio: ['https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'],
+    key: 'C'
+  },
+];
+//console.log(buttonInfo[6].id[0]);
+export default buttonInfo;
